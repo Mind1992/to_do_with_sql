6da07455f7e8d7b2749374c205a8c55e3f44009a
Jeff Stringer & Sergii Mertsalov <student@epicodus.com>
2014-08-05 09:51:37 -0700

    add .all method to List class

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -20,4 +20,24 @@ describe List do
     list = List.new('Clean')
     expect(list.name).to eq 'Clean'
   end
+
+  it 'is the same list if it has the same name' do
+    list1 = List.new('Clean')
+    list2 = List.new('Clean')
+    expect(list1).to eq list2
+  end
+
+  it 'lets you save lists' do
+    list1 = List.new('Clean')
+    list1.save
+    expect(List.all).to eq [list1]
+  end
+
+  it 'lets you have multiple lists' do
+    list1 = List.new('Clean')
+    list2 = List.new('Epicodus stuff')
+    list1.save
+    list2.save
+    expect(List.all).to eq [list1,list2]
+  end
 end
